64844b75522410ae4eafadfe2cf42d59fd79baf1
Haojun Zou <apollo_zhj@msn.com>
2017-03-29 08:15:33 +0200

    website_crm_campaign: remove controller

--- website_crm_campaign/campaign.py
+++ website_crm_campaign/campaign.py
@@ -23,10 +23,7 @@ from openerp import http
 from openerp.http import request
 import werkzeug
 from openerp.addons.website.controllers.main import Website
-from openerp.addons.website_sale.controllers.main import website_sale, QueryURL, table_compute
 import datetime
-PPG = 20 # Products Per Page
-PPR = 4  # Products Per Row
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -66,82 +63,3 @@ class product_public_category(models.Model):
 
     description = fields.Text(string='Description')
     mobile_icon = fields.Char(string='Mobile Icon', help='This icon will display on smaller devices')
-
-class website_campaign(Website):
-    @http.route('/', type='http', auth="public", website=True)
-    def index(self, **kw):
-        res = super(website_campaign, self).index(**kw)
-        campaign = request.env['crm.tracking.campaign'].get_campaigns()
-        if len(campaign) > 0:
-            return werkzeug.utils.redirect('/campaign', 302)
-        else:
-            return res
-
-class website_sale(website_sale):
-    @http.route([
-        '/campaign',
-        '/campaign/<model("crm.tracking.campaign"):campaign>',
-    ], type='http', auth="public", website=True)
-    def campaign_shop(self, page=0, category=None, campaign=None, search='', **post):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-
-        attrib_list = request.httprequest.args.getlist('attrib')
-        attrib_values = [map(int, v.split("-")) for v in attrib_list if v]
-        attrib_set = set([v[1] for v in attrib_values])
-        if attrib_list:
-            post['attrib'] = attrib_list
-
-        domain = self._get_search_domain(search, category, attrib_values)
-
-        keep = QueryURL('/campaign', category=category and int(category), search=search, attrib=attrib_list)
-
-        url = "/campaign"
-        product_count = request.env['product.template'].search_count(domain)
-        if search:
-            post["search"] = search
-        if category:
-            category = request.env['product.public.category'].browse(int(category))
-            url = "/shop/category/%s" % slug(category)
-
-        pager = request.website.pager(url=url, total=product_count, page=page, step=PPG, scope=7, url_args=post)
-        campaign = request.env['crm.tracking.campaign'].get_campaigns()
-        if not campaign:
-            return werkzeug.utils.redirect('/', 302)
-        campaign.ensure_one()
-
-        #~ if campaign:
-            #~ products = self.get_products(campaign.object_ids)
-        #~ else:
-            #~ campaign = request.env['crm.tracking.campaign'].search([('date_start', '<=', fields.Date.today()), ('date_stop', '>=', fields.Date.today()), ('website_published', '=', True)])
-            #~ if not campaign:
-                #~ return werkzeug.utils.redirect('/', 302)
-            #~ campaign = campaign[0]
-            #~ products = self.get_products(campaign.object_ids)
-
-        styles = request.env['product.style'].search([])
-        categs = request.env['product.public.category'].search([('parent_id', '=', False)])
-        attributes = request.env['product.attribute'].search([])
-
-        from_currency = request.env['product.price.type']._get_field_currency('list_price')
-        to_currency = pricelist.currency_id
-        compute_currency = lambda price: request.env['res.currency']._compute(from_currency, to_currency, price)
-
-        return request.website.render("website_sale.products", {
-            'search': search,
-            'category': category,
-            'attrib_values': attrib_values,
-            'attrib_set': attrib_set,
-            'pager': pager,
-            'pricelist': campaign.get_pricelist(),
-            'products': campaign.product_ids,
-            'bins': table_compute().process(campaign.product_ids),
-            'rows': PPR,
-            'styles': styles,
-            'categories': categs,
-            'attributes': attributes,
-            'compute_currency': compute_currency,
-            'keep': keep,
-            'style_in_product': lambda style, product: style.id in [s.id for s in product.website_style_ids],
-            'attrib_encode': lambda attribs: werkzeug.url_encode([('attrib',i) for i in attribs]),
-            'campaign': campaign,
-        })
