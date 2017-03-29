299a0d86a8516cd2483313c98746962a44926c8f
Haojun Zou <apollo_zhj@msn.com>
2017-03-29 08:09:12 +0200

    crm_campaign_product: move out pricelist and reseller

--- crm_campaign_product/product.py
+++ crm_campaign_product/product.py
@@ -26,8 +26,6 @@ _logger = logging.getLogger(__name__)
 class crm_tracking_campaign(models.Model):
     _inherit = 'crm.tracking.campaign'
 
-    pricelist = fields.Many2one(comodel_name='product.pricelist', string='Pricelist')
-    reseller_pricelist = fields.Many2one(comodel_name='product.pricelist', string='Reseller Pricelist')
     @api.one
     def _product_ids(self):
         products = self.env['product.template'].browse([])
@@ -41,19 +39,6 @@ class crm_tracking_campaign(models.Model):
         self.product_ids = products
     product_ids = fields.Many2many(comodel_name='product.template', compute='_product_ids',string='Products')
 
-    @api.multi
-    def get_campaigns(self):
-        return super(crm_tracking_campaign, self).get_campaigns().filtered(lambda c: c.reseller_pricelist or c.pricelist)
-
-    @api.multi
-    def get_pricelist(self):
-        self.ensure_one()
-        return None
-        #~ if not context.get('pricelist'):
-            #~ pricelist = self.get_pricelist()
-            #~ context['pricelist'] = int(pricelist)
-        #~ else:
-            #~ pricelist = request.env['product.pricelist'].browse(request.context['pricelist'])
 
 class product_template(models.Model):
     _inherit = 'product.template'
@@ -63,39 +48,6 @@ class product_template(models.Model):
     def _campaign_ids(self):
         self.campaign_ids = [(6, 0, self.env['crm.campaign.object'].search([('object_id', '=', self.id)]).filtered(lambda o: o.object_id._name == 'product.template').mapped('campaign_id').mapped('id'))]
     campaign_ids = fields.Many2many(comodel_name='crm.tracking.campaign', string='Campaign')
-
-class res_partner(models.Model):
-    _inherit = 'res.partner'
-
-    @api.model
-    def default_pricelist(self):
-        return self.env.ref('product.list0')
-    partner_product_pricelist = fields.Many2one(comodel_name='product.pricelist', domain=[('type','=','sale')], string='Sale Pricelist', help="This pricelist will be used, instead of the default one, for sales to the current partner", default=default_pricelist)
-    property_product_pricelist = fields.Many2one(comodel_name='product.pricelist', string='Sale Pricelist', compute='get_pricelist')
-
-    @api.one
-    def get_pricelist(self):
-        pricelist = self.partner_product_pricelist
-        if pricelist:
-            if pricelist.is_fixed:
-                self.property_product_pricelist = pricelist
-            else:
-                current_campaign = self.env['crm.tracking.campaign'].get_campaigns()
-                if len(current_campaign) > 0:
-                    if pricelist.is_reseller:
-                        self.property_product_pricelist = current_campaign[0].reseller_pricelist.id if current_campaign[0].reseller_pricelist else current_campaign[0].pricelist.id
-                    else:
-                        self.property_product_pricelist = current_campaign[0].pricelist.id if current_campaign[0].pricelist else pricelist
-                else:
-                    self.property_product_pricelist = pricelist
-        else:
-            self.property_product_pricelist = None
-
-class product_pricelist(models.Model):
-    _inherit = 'product.pricelist'
-
-    is_reseller = fields.Boolean(string='Reseller')
-    is_fixed = fields.Boolean(string='Fixed')
 
 
 class crm_campaign_object(models.Model):
