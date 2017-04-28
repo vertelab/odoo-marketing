68f84ac59c3ea92b1337f436455a7759b8987475
Haojun Zou <apollo_zhj@msn.com>
2017-04-28 11:06:03 +0200

    website_crm_campaign_runex: different views

--- website_crm_campaign_runex/campaign.py
+++ website_crm_campaign_runex/campaign.py
@@ -23,6 +23,7 @@ from openerp import http
 from openerp.http import request
 import werkzeug
 from openerp.addons.website.controllers.main import Website
+from openerp.addons.website.models.website import slug
 from openerp.addons.website_sale.controllers.main import website_sale, QueryURL, table_compute
 import datetime
 PPG = 20 # Products Per Page
@@ -149,7 +150,15 @@ class website_sale(website_sale):
             category = request.env['product.public.category'].browse(int(category))
             url = "/shop/category/%s" % slug(category)
 
-        pager = request.website.pager(url=url, total=product_count, page=page, step=PPG, scope=7, url_args=post)
+        ppg = PPG
+        if post.get('limit'):
+            limit = post.get('limit')
+            try:
+                int(limit)
+                ppg = abs(int(limit))
+            except:
+                pass
+        pager = request.website.pager(url=url, total=product_count, page=page, step=ppg, scope=7, url_args=post)
         campaign = request.env['crm.tracking.campaign'].with_context(context).get_campaigns()
         if not campaign:
             return werkzeug.utils.redirect('/', 302)
@@ -171,6 +180,10 @@ class website_sale(website_sale):
         from_currency = request.env['product.price.type']._get_field_currency('list_price', context)
         to_currency = pricelist.currency_id
         compute_currency = lambda price: request.env['res.currency']._compute(from_currency, to_currency, price)
+        campaign_products = campaign.product_ids.sorted(key=lambda p: p.name)
+        view_type = 'grid_view'
+        if post.get('view_type') and post.get('view_type') == 'list_view':
+            view_type = 'list_view'
 
         return request.website.render("website_sale.products", {
             'search': search,
@@ -179,7 +192,7 @@ class website_sale(website_sale):
             'attrib_set': attrib_set,
             'pager': pager,
             'pricelist': pricelist,
-            'products': campaign.product_ids,
+            'products': campaign_products,
             'bins': table_compute().process(campaign.product_ids),
             'rows': PPR,
             'styles': styles,
@@ -190,4 +203,97 @@ class website_sale(website_sale):
             'style_in_product': lambda style, product: style.id in [s.id for s in product.website_style_ids],
             'attrib_encode': lambda attribs: werkzeug.url_encode([('attrib',i) for i in attribs]),
             'campaign': campaign,
+            'product_count': product_count,
+            'view_type': view_type,
+            'url': url,
         })
+
+    @http.route([
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>'
+    ], type='http', auth="public", website=True)
+    def shop(self, page=0, category=None, search='', **post):
+        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [map(int, v.split("-")) for v in attrib_list if v]
+        attrib_set = set([v[1] for v in attrib_values])
+
+        domain = self._get_search_domain(search, category, attrib_values)
+
+        keep = QueryURL('/shop', category=category and int(category), search=search, attrib=attrib_list)
+
+        if not context.get('pricelist'):
+            pricelist = self.get_pricelist()
+            context['pricelist'] = int(pricelist)
+        else:
+            pricelist = pool.get('product.pricelist').browse(cr, uid, context['pricelist'], context)
+
+        product_obj = pool.get('product.template')
+
+        url = "/shop"
+        product_count = product_obj.search_count(cr, uid, domain, context=context)
+        if search:
+            post["search"] = search
+        if category:
+            category = pool['product.public.category'].browse(cr, uid, int(category), context=context)
+            url = "/shop/category/%s" % slug(category)
+        if attrib_list:
+            post['attrib'] = attrib_list
+
+        ppg = PPG
+        if post.get('limit'):
+            limit = post.get('limit')
+            try:
+                int(limit)
+                ppg = abs(int(limit))
+            except:
+                pass
+        pager = request.website.pager(url=url, total=product_count, page=page, step=ppg, scope=7, url_args=post)
+        product_ids = product_obj.search(cr, uid, domain, limit=ppg, offset=pager['offset'], order=self._get_search_order(post), context=context)
+        products = product_obj.browse(cr, uid, product_ids, context=context)
+
+        style_obj = pool['product.style']
+        style_ids = style_obj.search(cr, uid, [], context=context)
+        styles = style_obj.browse(cr, uid, style_ids, context=context)
+
+        category_obj = pool['product.public.category']
+        category_ids = category_obj.search(cr, uid, [('parent_id', '=', False)], context=context)
+        categs = category_obj.browse(cr, uid, category_ids, context=context)
+
+        attributes_obj = request.registry['product.attribute']
+        attributes_ids = attributes_obj.search(cr, uid, [], context=context)
+        attributes = attributes_obj.browse(cr, uid, attributes_ids, context=context)
+
+        from_currency = pool.get('product.price.type')._get_field_currency(cr, uid, 'list_price', context)
+        to_currency = pricelist.currency_id
+        compute_currency = lambda price: pool['res.currency']._compute(cr, uid, from_currency, to_currency, price, context=context)
+        products = products.sorted(key=lambda p: p.name)
+        view_type = 'grid_view'
+        if post.get('view_type') and post.get('view_type') == 'list_view':
+            view_type = 'list_view'
+        _logger.warn(product_count)
+        values = {
+            'search': search,
+            'category': category,
+            'attrib_values': attrib_values,
+            'attrib_set': attrib_set,
+            'pager': pager,
+            'pricelist': pricelist,
+            'products': products,
+            'bins': table_compute().process(products),
+            'rows': PPR,
+            'styles': styles,
+            'categories': categs,
+            'attributes': attributes,
+            'compute_currency': compute_currency,
+            'keep': keep,
+            'style_in_product': lambda style, product: style.id in [s.id for s in product.website_style_ids],
+            'attrib_encode': lambda attribs: werkzeug.url_encode([('attrib',i) for i in attribs]),
+            'product_count': product_count,
+            'view_type': view_type,
+            'url': url,
+        }
+        return request.website.render("website_sale.products", values)
