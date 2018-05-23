e2e93514a37b8fbc7c6a15d32421faa119b9a7c0
Robin Chatfield <robin.chatfield@vertel.se>
2018-05-23 11:30:04 +0200

    crm_campaign_phase: Fixed a bug due to access rights in price_get.

--- crm_campaign_phase/crm.py
+++ crm_campaign_phase/crm.py
@@ -135,7 +135,7 @@ class product_pricelist(models.Model):
             partner = partner.id
         else:
             partner = self.sudo().env.ref('base.public_partner').id
-        is_reseller = self.sudo().env.ref('base.public_user').property_product_pricelist != self.env['res.partner'].browse(partner).property_product_pricelist
+        is_reseller = self.sudo().env.ref('base.public_user').property_product_pricelist != self.env['res.partner'].sudo().browse(partner).property_product_pricelist
         price = [p[0] for key, p in self.sudo().price_rule_get(prod_id, qty, partner=partner).items()][0]
         campaign_price = 999999999999999999999999999999.0
         date = self.env.context['date'] if self.env.context.get('date') else fields.Date.today()
